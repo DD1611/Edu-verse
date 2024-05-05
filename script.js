8ecc8536cef46e16ebc3be7b8cf9dfafa2750f77
Dhruvil Dave <146173104+DD1611@users.noreply.github.com>
2024-05-05 12:19:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+function Dashboardfun(){
+    window.location.href = "./Dashboard.html";
+}
+function Indexfun(){
+    window.location.href = "./index.html";
+}
+function createfun(){
+    window.location.href = "./create.html" ;
+}
+function notesfun(){
+    window.location.href = "./Notes.html";
+}
+function videosfun(){
+    window.location.href = "./Videos.html";
+}
+function assignmentfun(){
+    window.location.href = "./Assignment.html";
+}
+function profilefun(){
+    window.location.href = "./profile.html";
+}
+function indexfun(){
+    window.location.href = "./index.html";
+}
+function pdfnumbers(){
+    window.open("https://drive.google.com/file/d/1yHrL_zCvvLSlCTxdp5ldbvXWJ_pT0gDj/view?usp=drive_link");
+}
+function pdfshapes(){
+    window.open("https://drive.google.com/file/d/1mmLxanxYG0BrX9cv1PqhoHC9LNMUv1jD/view?usp=sharing");
+}
+function pdfalphabets(){
+    window.open("https://drive.google.com/file/d/1iw-rLefSzbeKus0SLipWKN6CqrMP3zTb/view?usp=sharing");
+}
+function assnumbers(){
+    window.open("https://drive.google.com/file/d/1jFlMwHSXCCfnVxnQHvw8XrT2C0uOeC7D/view?usp=sharing");
+}
+function assnumbers2(){
+    window.open("https://drive.google.com/file/d/1p3dUsUCVzzXXWJNPbtEYqIZDkpBod7HU/view?usp=sharing");
+}
+function assnumbers3(){
+    window.open("https://drive.google.com/file/d/1FzaFbOo8Yu9albLAoZbYRfdznVD45y7f/view?usp=sharing");
+}
+function assshapes(){
+    window.open("https://drive.google.com/file/d/1SRircguteKK4JSVYtrL45_lX7eA1SzoH/view?usp=sharing");
+}
+function assalphabets(){
+    window.open("https://drive.google.com/file/d/1Z3jaXlgYKtDt5Nbdp366gzFxHJniYBZ6/view?usp=sharing");
+}
+function assalphabets2(){
+    window.open("https://drive.google.com/file/d/1-E6zfb3hjiLUU3V98mvfjUNfnl2wpEdi/view?usp=sharing");
+}
+function openFullscreen() {
+ if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+}
+}
+
